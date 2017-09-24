98ccd18a04e95c24582939e059249a282acbc8f1
Mark Adam <mark.adam4292@gmail.com>
2017-09-24 13:59:13 +0200

    Добавлен мир

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ class HelloWorldRunner
     private game: Phaser.Game;
     private width: number;
     private height: number;
-    private earth: Phaser.Sprite;
+    //private earth: Phaser.Sprite;
+    private grass: Phaser.Sprite;
+    private box: Phaser.Sprite;
     private player: Phaser.Sprite;
     private playerIsWalking: boolean = false;
 
@@ -41,19 +43,28 @@ class HelloWorldRunner
 
     preload(): void
     {
-        this.game.load.image('earth', 'texture/earth.png');
+        //this.game.load.image('earth', 'texture/earth.png');
+        this.game.load.image('grass', 'texture/grass.png');
+        this.game.load.image('box', 'texture/box.png');
         this.game.load.spritesheet('player.walk', 'texture/player_walks.png', 128, 128, 20);
     }
 
     create(): void
     {
-        this.earth = this.game.add.sprite(100, 100, 'earth');
+        this.game.add.tileSprite(0, 0, 4*window.innerWidth,4*window.innerHeight, 'grass');
+        this.game.world.setBounds(0, 0, 4*window.innerWidth,4*window.innerHeight);
+        for (var i = 0; i < 350; i++)
+          {
+            this.game.add.sprite(this.game.world.randomX, this.game.world.randomY, 'box');
+          }
+        //this.earth = this.game.add.sprite(100, 100, 'earth');
         this.player = this.game.add.sprite(64, 64, 'player.walk');
         this.player.animations.add('player.walk');
         this.player.anchor.set(0.5, 0.5);
-        this.earth.anchor.set(0.5, 0.5);
+        //this.earth.anchor.set(0.5, 0.5);
 
         this.player.animations.play('player.walk', 20, true);
+        this.game.camera.follow(this.player);
 
         // TODO: добавить отдельный класс реализующий прослойку между управлением и Phaser.Input
         this.game.input.addMoveCallback((pointer, x, y) => {
@@ -70,35 +81,35 @@ class HelloWorldRunner
             }
             this.player.rotation = angle;
 
-            // console.log(lookAtVector.x, lookAtVector.y);
+            console.log(lookAtVector.x, lookAtVector.y);
             console.log(Math.round(MathFunctions.radiansToDegrees(this.player.rotation)));
         }, null);
     }
 
     update(): void
     {
-        this.earth.rotation += 0.01;
+        //this.earth.rotation += 0.01;
         let playerIsWalking = false;
 
         // TODO: вынести в отдельную прослойку
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.W))
         {
-            this.player.y -= 2;
+            this.player.y -= 10;
             playerIsWalking = true;
         }
         else if (this.game.input.keyboard.isDown(Phaser.Keyboard.S))
         {
-            this.player.y += 2;
+            this.player.y += 10;
             playerIsWalking = true;
         }
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.D))
         {
-            this.player.x += 2;
+            this.player.x += 10;
             playerIsWalking = true;
         }
         else if (this.game.input.keyboard.isDown(Phaser.Keyboard.A))
         {
-            this.player.x -= 2;
+            this.player.x -= 10;
             playerIsWalking = true;
         }
 
